cc5f59e5b1cb832d20b93a2a36f1ebde55a1f2b4
Cydeo-Mehtap <mehtapt2009@gmail.com>
2024-03-20 10:08:38 -0500

    added 2 more locators inside MessagePage.java

--- src/test/java/com/crm/pages/MessagePage.java
+++ src/test/java/com/crm/pages/MessagePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class MessagePage {
 
     public MessagePage() {
@@ -46,6 +48,14 @@ public class MessagePage {
 
     @FindBy(xpath = "//*[contains(@id, 'bxid')]")
     public WebElement fileInText;
+
+    @FindBy(xpath = "//span[contains(@id, 'check-in-text')]")
+    public List<WebElement> insertInTextButtons;
+
+
+    @FindBy(xpath = "//*[contains(@id, 'bxid')]")
+    public List<WebElement> filesInText;
+
 
 
 }
