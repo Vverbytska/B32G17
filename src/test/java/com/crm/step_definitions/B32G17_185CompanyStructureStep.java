e5c6e00f52c7fd5bb7598006cc2075ec7e800801
linaalrawi <linaessam81@gmail.com>
2024-03-20 08:52:40 -0500

    Just Creat Step Definition185

---
+++ src/test/java/com/crm/step_definitions/B32G17_185CompanyStructureStep.java
@@ -0,0 +1,52 @@
+package com.crm.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class B32G17_185CompanyStructureStep {
+    @Given("user is on the login page")
+    public void userIsOnTheLoginPage() {
+    }
+
+    @When("user enters the {string} information")
+    public void userEntersTheInformation(String arg0) {
+    }
+
+    @Then("user should be able to login")
+    public void userShouldBeAbleToLogin() {
+    }
+
+    @When("the user navigates to the Employee menu")
+    public void theUserNavigatesToTheEmployeeMenu() {
+    }
+
+    @Then("the Company Structure option should be visible")
+    public void theCompanyStructureOptionShouldBeVisible() {
+    }
+
+    @Given("that user is already on the homepage")
+    public void thatUserIsAlreadyOnTheHomepage() {
+    }
+
+    @When("user clicks the {string}")
+    public void userClicksThe(String arg0) {
+    }
+
+    @Then("user should be able to see {string} as default.")
+    public void userShouldBeAbleToSeeAsDefault(String arg0) {
+    }
+
+    @Given("the user type is {string}")
+    public void theUserTypeIs(String arg0) {
+    }
+
+    @And("the user is on the Company Structure page")
+    public void theUserIsOnTheCompanyStructurePage() {
+    }
+
+    @Then("the {string} button should not be displayed")
+    public void theButtonShouldNotBeDisplayed(String arg0) {
+    }
+}//new.......
